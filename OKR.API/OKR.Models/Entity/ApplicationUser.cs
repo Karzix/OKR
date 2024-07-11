be047a0a35959c4a391280afce9a270f7a6ef611
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2024-07-11 14:41:55 +0700

    s

--- OKR.API/OKR.Models/Entity/ApplicationUser.cs
+++ OKR.API/OKR.Models/Entity/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace OKR.Models.Entity
 {
     public class ApplicationUser : IdentityUser
     {
-
+        [ForeignKey("Department")]
+        public Guid? DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public Department? Department { get; set; }
     }
 }
---
+++ OKR.API/OKR.Models/Migrations/20240711064205_addDepartmentIdForUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OKR.Models.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDepartmentIdForUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartmentId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_DepartmentId",
+                table: "AspNetUsers",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Department_DepartmentId",
+                table: "AspNetUsers",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Department_DepartmentId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_DepartmentId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "AspNetUsers");
+        }
+    }
+}
