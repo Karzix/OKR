3a528708556991fe3cb958f7097a0d6d50aaade6
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2024-07-19 10:32:04 +0700

    a

---
+++ OKR.API/OKR.Repository/Contract/IUseRepository.cs
@@ -0,0 +1,16 @@
+﻿using MayNghien.Models.Response.Base;
+using OKR.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OKR.Repository.Contract
+{
+    public interface IUseRepository 
+    {
+        public IQueryable FindBy(Expression<Func<ApplicationUser, bool>> predicate);
+    }
+}
---
+++ OKR.API/OKR.Repository/Implementation/UseRepository.cs
@@ -0,0 +1,28 @@
+﻿using OKR.Models.Context;
+using OKR.Models.Entity;
+using OKR.Repository.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OKR.Repository.Implementation
+{
+    public class UseRepository : IUseRepository
+    {
+        private OKRDBContext _context;
+
+
+        public UseRepository(OKRDBContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable FindBy(Expression<Func<ApplicationUser, bool>> predicate)
+        {
+            return _context.Users.Where(predicate);
+        }
+    }
+}
