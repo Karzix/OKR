7260f5e7248869d2cc746a1ae956b4ae60178337
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2024-07-19 16:06:52 +0700

    a

---
+++ OKR.UI/src/components/maynghien/Common/handleSearchFilter.ts
@@ -0,0 +1,44 @@
+import {Filter} from "@/components/maynghien/BaseModels/Filter";
+function isString(value: any) {
+    return typeof value === 'string';
+}
+export function addFilter(listFilter : Filter[], add : Filter){
+    var findFilName = listFilter.find(x => x.FieldName == add.FieldName);
+    if(findFilName == null){
+        listFilter.push(add);
+    }
+    else{
+        findFilName.Value = add.Value;
+    }
+}
+
+export function removeFilter(listFilter : Filter[], remove : Filter){
+    var findFilName = listFilter.find(x => x.FieldName == remove.FieldName);
+    if(findFilName != null){
+        listFilter = listFilter.filter(x => x.FieldName != remove.FieldName);
+    }
+}
+
+export function handleFilterBeforSearch(listFilter : Filter[]) : Filter[]{
+    var result = [] as Filter[];
+    listFilter.forEach(x => {
+        var newf = new Filter();
+        newf.FieldName == x.FieldName;
+        // newf.Value == x.Value;
+        newf.Operation == x.Operation;
+        newf.Type == x.Type;
+        newf.dropdownData == x.dropdownData;
+        newf.DisplayName == x.DisplayName;
+
+        if(x.Value != null && x.Value != undefined && x.Value != ""){
+            if(!isString(x.Value)){
+                newf.Value = (x.Value as string).toString();
+            }
+            else{
+                newf.Value = x.Value;
+            }
+            result.push(newf);
+        }
+    });
+    return result;
+}
