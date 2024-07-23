cca3262e5f6de447bdb5cb4a62cb982edf33989f
Kiệt Nguyễn Anh <karzix1809@gmail.com>
2024-07-23 17:25:44 +0700

    a

--- OKR.UI/src/components/Layout/layout1.vue
+++ OKR.UI/src/components/Layout/layout1.vue
@@ -1,42 +1,116 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-aside >
-        <el-row class="tac">
-          <el-col :span="30">
-            <el-menu default-active="1" class="el-menu-vertical-demo">
-              <el-menu-item index="8" @click="handleAsideClick('logout')">
-                <el-icon>
-                  <Close />
-                </el-icon>
-                <span>{{ Cookies.get('userName')?.toString() }}</span>
-              </el-menu-item>
-            </el-menu>
-          </el-col>
-        </el-row>
-      </el-aside>
+      <div id="aside">
+        <el-aside v-if="!isMobile" v-show="isAsideVisible">
+          <el-row class="tac">
+            <el-col>
+              <el-menu default-active="1" class="el-menu-vertical-demo">
+                <el-menu-item index="1" @click="handleAsideClick('logout')">
+                  <el-icon>
+                    <Close />
+                  </el-icon>
+                  <span>{{ Cookies.get("userName")?.toString() }}</span>
+                </el-menu-item>
+                <el-menu-item index="2" @click="handleAsideClick('User')">
+                  <el-icon>
+                    <Close />
+                  </el-icon>
+                  <span>User</span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-aside>
+
+        <el-drawer
+          v-model="drawerMenuMobile"
+          width="100%"
+          direction="ltr"
+        >
+          <el-row class="tac">
+            <el-col>
+              <el-menu default-active="1" class="el-menu-vertical-demo">
+                <el-menu-item index="1" @click="handleAsideClick('logout')">
+                  <el-icon>
+                    <Close />
+                  </el-icon>
+                  <span>{{ Cookies.get("userName")?.toString() }}</span>
+                </el-menu-item>
+                <el-menu-item index="2" @click="handleAsideClick('User')">
+                  <el-icon>
+                    <Close />
+                  </el-icon>
+                  <span>User</span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-drawer>
+      </div>
+
       <el-container>
-        <el-header>Header</el-header>
-        <el-main> <router-view /></el-main>
+        <el-header>
+          <!-- <button class="toggle-button" >☰</button> -->
+          Header
+          <el-button v-if="isMobile" class="toggle-button" type="primary" :icon="Expand" circle @click="toggleAside"/>
+        </el-header>
+        <el-main>
+          <router-view />
+        </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
 import router from "@/router";
-import Cookies from 'js-cookie';
-import {Close} from '@element-plus/icons-vue'
+import Cookies from "js-cookie";
+import { Close,Expand } from "@element-plus/icons-vue";
+
+const isAsideVisible = ref(true);
+const isMobile = ref(false);
+const drawerMenuMobile = ref(false);
+
 const handleAsideClick = (action: string) => {
   switch (action) {
     case "logout":
-        logout()
+      logout();
       break;
-
+    case "User":
+      router.push("/user");
+      break;
     default:
       break;
   }
 };
+
+const toggleAside = () => {
+  if (isMobile.value) {
+    drawerMenuMobile.value = !drawerMenuMobile.value;
+  } else {
+    isAsideVisible.value = !isAsideVisible.value;
+  }
+};
+
+const handleResize = () => {
+  isMobile.value = window.innerWidth < 600;
+  if (!isMobile.value) {
+    isAsideVisible.value = true;
+  }
+  console.log(isAsideVisible.value);
+};
+
+onMounted(() => {
+  window.addEventListener("resize", handleResize);
+  handleResize(); // Gọi hàm này để kiểm tra kích thước ban đầu
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", handleResize);
+});
+
 function logout() {
   var cookies = document.cookie.split(";");
 
@@ -49,3 +123,33 @@ function logout() {
   window.location.reload();
 }
 </script>
+
+<style>
+.el-aside {
+  background-color: rgb(5, 51, 111);
+  height: 100vh;
+}
+
+.toggle-button {
+  display: none;
+}
+@media (min-width: 600px) {
+  .toggle-button {
+  display: none;
+}
+}
+@media (max-width: 600px) {
+  .toggle-button {
+    display: block;
+  }
+  .el-drawer.ltr {
+    width: 80% !important;
+  }
+  .el-aside{
+    width: 100%;
+  }
+  .el-drawer__body {
+    padding: 0 !important;
+  }
+}
+</style>
