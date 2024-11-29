82938891b06e1f8ff6e94083f81cdb21ca3a3a08
Kiệt Nguyễn Anh <karzix1809@gmail.com>
2024-11-29 20:16:52 +0700

    add col isLocked for user

---
+++ OKR.API/OKR.API/StartUp/CustomUserManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using OKR.Models.Entity;
+
+namespace OKR.API.StartUp
+{
+    public class CustomUserManager : UserManager<ApplicationUser>
+    {
+        public CustomUserManager(IUserStore<ApplicationUser> store, IOptions<IdentityOptions> optionsAccessor,
+                                 IPasswordHasher<ApplicationUser> passwordHasher,
+                                 IEnumerable<IUserValidator<ApplicationUser>> userValidators,
+                                 IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators,
+                                 ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors,
+                                 IServiceProvider services, ILogger<UserManager<ApplicationUser>> logger)
+            : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators,
+                   keyNormalizer, errors, services, logger)
+        { }
+
+        public override async Task<bool> CheckPasswordAsync(ApplicationUser user, string password)
+        {
+            if (user.IsLocked)
+            {
+                throw new Exception("User is locked");
+            }
+
+            return await base.CheckPasswordAsync(user, password);
+        }
+    }
+}
---
+++ OKR.API/OKR.Models/Migrations/20241129130438_addColIsLockedForUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OKR.Models.Migrations
+{
+    /// <inheritdoc />
+    public partial class addColIsLockedForUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocked",
+                table: "AspNetUsers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLocked",
+                table: "AspNetUsers");
+        }
+    }
+}
